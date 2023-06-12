2fac90e39bf77a0139810f56cae95ad4f199e277
SyrineGhl <syrine.ghoul@gmail.com>
2023-06-12 16:04:00 +0200

    banner css

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,7 @@
 import { BrowserRouter as Router, Routes, Route, Navigate} from 'react-router-dom';
 import Home from './pages/home';
 import APropos from './pages/a_propos_logement';
-import FicheLogement from './pages/fiche_logement';
+import FicheLogement from './pages/PropertyInfo';
 import QuatreCentQuatre from './pages/404';
 
 function App() {
