62c0e9e34feaa3413106fd808c5393f6a907b3a8
SyrineGhl <syrine.ghoul@gmail.com>
2023-06-26 11:36:56 +0200

    fix slideshow

--- frontend/src/components/slideshow.jsx
+++ frontend/src/components/slideshow.jsx
@@ -19,37 +19,39 @@ const Slideshow = ({ images }) => {
     return null;
   }
 
+  const showArrows = images.length > 1;
+
   return (
     <div className="slideshow">
-      <button
-        className="slideshow-button prev-button"
-        onClick={handlePreviousImage} aria-label="Afficher les images précédentes"
-      >
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          height="1em"
-          viewBox="0 0 320 512"
+      {showArrows && (
+        <button
+          className="slideshow-button prev-button"
+          onClick={handlePreviousImage}
+          aria-label="Afficher les images précédentes"
         >
-          <path d="M9.4 233.4c-12.5 12.5-12.5 32.8 0 45.3l192 192c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3L77.3 256 246.6 86.6c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0l-192 192z" />
-        </svg>
-      </button>
+          <svg xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 320 512">
+            <path d="M9.4 233.4c-12.5 12.5-12.5 32.8 0 45.3l192 192c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3L77.3 256 246.6 86.6c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0l-192 192z" />
+          </svg>
+        </button>
+      )}
+      {showArrows && (
+        <button
+          className="slideshow-button next-button"
+          onClick={handleNextImage}
+          aria-label="Afficher les images suivantes"
+        >
+          <svg xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 320 512">
+            <path d="M310.6 233.4c12.5 12.5 12.5 32.8 0 45.3l-192 192c-12.5 12.5-32.8 12.5-45.3 0s-12.5-32.8 0-45.3L242.7 256 73.4 86.6c-12.5-12.5-12.5-32.8 0-45.3s32.8-12.5 45.3 0l192 192z" />
+          </svg>
+        </button>
+      )}
       <img
         className="slideshow-image"
         src={images[currentImageIndex]}
-        alt="Slideshow" width={984} height={415}
+        alt="Slideshow"
+        width={984}
+        height={415}
       />
-      <button
-        className="slideshow-button next-button"
-        onClick={handleNextImage} aria-label="Afficher les images suivantes"
-      >
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          height="1em"
-          viewBox="0 0 320 512"
-        >
-          <path d="M310.6 233.4c12.5 12.5 12.5 32.8 0 45.3l-192 192c-12.5 12.5-32.8 12.5-45.3 0s-12.5-32.8 0-45.3L242.7 256 73.4 86.6c-12.5-12.5-12.5-32.8 0-45.3s32.8-12.5 45.3 0l192 192z" />
-        </svg>
-      </button>
     </div>
   );
 };
